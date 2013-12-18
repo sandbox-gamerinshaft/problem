d20397e45f392dd8cddbcb27035e0e82a3081aaa
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-18 18:33:56 +0900

    scaffold quest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Problem::Application.config.secret_key_base = '9b154d73bd481d4a7c51c34818ab9aa326161c3ac84aaa971a6fb33fd63fc11f2ce72cf3e2988195a067698176e4030e0c3c6e83d63bb66ee59f8d5faab76f5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Problem::Application.config.session_store :cookie_store, key: '_problem_session'
