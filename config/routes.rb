00ad24fe08eb5b502d6f6ddf56898efb5c61d514
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-18 18:59:44 +0900

    setting relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Problem::Application.routes.draw do
-  resources :answers
-
-  resources :quests
-
+  resources :quests do
+    resources :answers
+  end  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
